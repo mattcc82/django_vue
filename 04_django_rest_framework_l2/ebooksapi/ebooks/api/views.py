7e2f19b953cbb8431dfedfc8d7bfe7cb52d9baa7
Matt Castle <mcastle@gongos.com>
2019-05-10 14:41:26 -0400

    - modified model and included simple permissions

--- 04_django_rest_framework_l2/ebooksapi/ebooks/api/views.py
+++ 04_django_rest_framework_l2/ebooksapi/ebooks/api/views.py
@@ -1,35 +1,60 @@
 from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 # from rest_framework import mixins
 
 
 from ebooks.models import Ebook, Review
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
-
+from ebooks.api.permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [
+        IsAdminUserOrReadOnly
+    ]
 
 
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [
+        IsAdminUserOrReadOnly
+    ]
 
 
 class ReviewCreateAPIView(generics.CreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get('ebook_pk')
         ebook = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+
+        review_author = self.request.user
+
+        review_queryset = Review.objects.filter(
+            ebook=ebook,
+            review_author=review_author
+        )
+
+        if review_queryset.exists():
+            raise ValidationError('You already reviewed this book')
+        
+        serializer.save(ebook=ebook, review_author=review_author)
 
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [
+        IsReviewAuthorOrReadOnly
+    ]
 
 
 # class EbookListCreateAPIView(mixins.ListModelMixin,
--- 04_django_rest_framework_l2/ebooksapi/ebooks/models.py
+++ 04_django_rest_framework_l2/ebooksapi/ebooks/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from django.contrib.auth.models import User
+
+
 class Ebook(models.Model):
     title = models.CharField(max_length=120)
     author = models.CharField(max_length=60)
@@ -14,7 +17,10 @@ class Ebook(models.Model):
 class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    review_author = models.CharField(max_length=8, blank=True, null=True)
+    review_author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
     review = models.TextField(blank=True, null=True)
     rating = models.PositiveIntegerField(
         validators=[
