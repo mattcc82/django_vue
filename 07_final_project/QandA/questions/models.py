8ee7ae4c7d833f3d7e5853c8c1b62ef180d21c0c
Matt Castle <mcastle@gongos.com>
2019-05-31 13:51:53 -0400

    created models for 'questions' app

--- 07_final_project/QandA/questions/models.py
+++ 07_final_project/QandA/questions/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='question')
+
+    def __str__(self):
+        return self.content
+
+
+class Answer(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='answer')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    voters = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='vote')
+
+    def __str__(self):
+        return self.author.username
